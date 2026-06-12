7dab3f0d488303dfd7bc9c9ccde3f24029284c39
Ishant921 <84021307+Ishant921@users.noreply.github.com>
2026-06-12 13:34:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 try {
-  const root = document.getElementById("root");
+  const root = ReactDOM.createRoot(
+    document.getElementById("root")
+  );
 
-  ReactDOM.createRoot(root);
+  root.render("Hello");
 
-  document.body.innerHTML += "<p>createRoot succeeded</p>";
+  document.body.innerHTML += "<p>render succeeded</p>";
 } catch (err) {
   document.body.innerHTML +=
     "<pre>" + err.toString() + "</pre>";

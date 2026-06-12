1109eaacf873c72cfc24ecc04c0b1e33a3da5e54
Ishant921 <ishantgoyal92@gmail.com>
2026-06-12 17:54:09 +0530

    test

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import HexGrid from "./HexGrid.jsx";
+// import React from "react";
+// import HexGrid from "./HexGrid.jsx";
 
-export default function App() {
-  return (
-    <div>
-      IMPORT WORKED
-      <HexGrid />
-    </div>
-  );
-}
+// export default function App() {
+//   return (
+//     <div>
+//       IMPORT WORKED
+//       <HexGrid />
+//     </div>
+//   );
+// }

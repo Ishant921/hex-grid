01f159e4425541fb05e32c40fae43bca170c0907
Ishant921 <84021307+Ishant921@users.noreply.github.com>
2026-06-12 13:26:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
+console.log("JS loaded");
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.body.innerHTML = "<h1>React imported successfully</h1>";
